bdf6178c69fdb09451450ff48e7c1a189259bdf2
AngelManuelLibrerosRoman <l21021342@veracruz.tecnm.mx>
2023-03-05 16:57:14 -0600

    Proyecto escolar inicial versión 1.4 Código de clase Materia Elaborado por: AMLR.dev

---
+++ src/Materia.java
@@ -0,0 +1,32 @@
+public class Materia {
+    private int id;
+    private String nombre;
+
+    public Materia(){
+    }
+
+    public Materia(int id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString(){
+        return "Materia{" + "Id: "+ id+ ", nombre: "+ nombre + "}";
+    }
+}
